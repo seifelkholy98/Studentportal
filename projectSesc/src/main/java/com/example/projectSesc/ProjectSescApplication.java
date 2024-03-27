7aff39ddbbe07598f17464bf0006578b826cd541
seifelkholy98 <seifmohamed728@gmail.com>
2024-03-27 10:41:56 +0200

    second commit

---
+++ projectSesc/src/main/java/com/example/projectSesc/ProjectSescApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectSesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSescApplication.class, args);
+	}
+
+}
---
+++ projectSesc/src/test/java/com/example/projectSesc/ProjectSescApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectSesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSescApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
